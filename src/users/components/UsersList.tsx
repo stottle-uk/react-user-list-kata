c88d44fa2e52c6cb7074f28ec63a180ef2778e8d
Stuart Tottle <stuarttottle@hotmail.com>
2019-10-11 15:05:26 +0100

    Sort usernames

--- src/users/components/UsersList.tsx
+++ src/users/components/UsersList.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { BaseUser } from '../models/User';
 import { UsersServiceContext } from '../services/UsersServiceContext';
 
@@ -7,12 +7,25 @@ const UsersList: React.FC = () => {
   const [usersData, setUsersData] = useState<BaseUser[]>([]);
   const { usersService } = useContext(UsersServiceContext);
 
+  const byUsername = (a: BaseUser, b: BaseUser) => {
+    if (a.username < b.username) {
+      return -1;
+    }
+    if (a.username > b.username) {
+      return 1;
+    }
+    return 0;
+  };
+
   const renderUser = (user: BaseUser) => <li key={user.id}>{user.username}</li>;
 
   const usersDataEffect = () => {
     const subscription = usersService
       .getAll()
-      .pipe(tap(users => setUsersData(users)))
+      .pipe(
+        map(users => users.sort(byUsername)),
+        tap(users => setUsersData(users))
+      )
       .subscribe();
     return () => subscription.unsubscribe();
   };
