d1a77a8dc442dccfaa5113b9ff1ad17305a02ae0
Stuart Tottle <stuarttottle@hotmail.com>
2019-10-11 14:39:19 +0100

    Added user model

---
+++ src/users/models/User.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  address: string;
+  county: string;
+  country: string;
+  postcode: string;
+}
--- src/users/services/UsersService.ts
+++ src/users/services/UsersService.ts
@@ -1,33 +1,35 @@
 import Axios from 'axios-observable';
 import { Observable, throwError, timer } from 'rxjs';
-import { delayWhen, retryWhen, scan } from 'rxjs/operators';
+import { delayWhen, map, retryWhen, scan } from 'rxjs/operators';
+import { User } from '../models/User';
 
 interface IGetUsers {
-  getAll: () => Observable<any>;
-  getById: (userId: string) => Observable<any>;
+  getAll: () => Observable<User[]>;
+  getById: (userId: string) => Observable<User>;
 }
 
 interface IUpdateUsers {
-  updateUser: (user: any) => Observable<any>;
+  updateUser: (user: Partial<User>) => Observable<User>;
 }
 
 export class UsersService implements IGetUsers, IUpdateUsers {
   private baseUrl = 'http://localhost:3000';
 
   getAll() {
-    return this.getWithRetry(`/users`);
+    return this.getWithRetry<User[]>(`/users`);
   }
 
-  getById() {
-    return new Observable();
+  getById(userId: string) {
+    return this.getWithRetry<User>(`/users/${userId}`);
   }
 
-  updateUser() {
-    return new Observable();
+  updateUser(user: Partial<User>) {
+    return Observable.create(user);
   }
 
-  private getWithRetry(url: string): Observable<any> {
-    return Axios.get(`${this.baseUrl}${url}`).pipe(
+  private getWithRetry<T>(url: string): Observable<T> {
+    return Axios.get<T>(`${this.baseUrl}${url}`).pipe(
+      map(response => response.data),
       retryWhen(errors =>
         errors.pipe(
           scan(errorCount => ++errorCount, 0),
