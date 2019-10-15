3fa0ec98722d24bcf5da7fb816f3656a1ed9bbf4
Stuart Tottle <stuarttottle@hotmail.com>
2019-10-15 13:19:34 +0100

    better message when api not running

--- src/users/components/UserErrors.tsx
+++ src/users/components/UserErrors.tsx
@@ -10,7 +10,7 @@ const UserErrors: React.FC<OwnProps> = ({ errors, retry, retryText }) =>
   !!errors.length ? (
     <article className="message is-danger is-in-modal">
       <div className="message-body">
-        {errors[errors.length - 1].data}
+        {errors[errors.length - 1] ? errors[errors.length - 1].data : 'Unexpected Error (is the API running?)'}
         {!!retry && <b onClick={retry}>&nbsp;{retryText}</b>}
       </div>
       {/* todo: show something nice when max retries fails */}
