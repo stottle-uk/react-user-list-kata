74d8631591d9bf1ea7e0c9dd118afa776df104bb
Stuart Tottle <stuarttottle@hotmail.com>
2019-10-17 16:39:24 +0100

    created script to run docker

---
+++ server/scripts/docker/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo
+echo ==================================================================
+echo  Building and running React-User-List-Kata
+echo ==================================================================
+echo
+
+# docker build -f ./src/MyIdentityServer/Dockerfile -t myidentityserver .
+docker build -f ./Dockerfile -t stottleuk/react-user-list-kata ../../
+
+# docker run --rm -d -p 8002:8002 --name my-identity-server myidentityserver
+docker run --name react-user-list-kata -p 8080:3102 -d --rm stottleuk/react-user-list-kata
