4ed93d7dc7cd342245762791d266784361de3852
Stuart Tottle <stuarttottle@hotmail.com>
2019-10-25 11:54:55 +0100

    added CarouselItems component

--- libs/templateEntries/components/HeroStandard3x1.tsx
+++ libs/templateEntries/components/HeroStandard3x1.tsx
@@ -1,36 +1,19 @@
-import { Item, List } from '@lists';
-import { Link } from '@router';
+import { List } from '@lists';
 import React from 'react';
-import Carousel from '../helpers/Caurosel';
-import './HeroStandard3x1.css';
+import Carousel from '../helpers/Carousel';
+import CarouselItems from '../helpers/CarouselItems';
 
 class HeroStandard3x1 extends React.PureComponent<List> {
   render() {
     return (
-      <Carousel className="swiper" intervalTime={2000}>
-        {this.props.items && this.renderCarouselItems(this.props.items)}
+      <Carousel className="carousel" intervalTime={2000}>
+        <CarouselItems
+          className="carousel__item image is-3by1"
+          items={this.props.items}
+        />
       </Carousel>
     );
   }
-
-  private renderCarouselItems(items: Item[]): React.ReactNodeArray {
-    return items.map(item => (
-      <div
-        className="swiper__item image is-3by1"
-        key={item.id}
-        style={this.setImage(item)}
-      >
-        <div className="item__title">
-          <Link href={item.path}>{item.title}</Link>
-          <p>{item.tagline}</p>
-        </div>
-      </div>
-    ));
-  }
-
-  private setImage = (item: Item) => ({
-    backgroundImage: `url("${item.images.hero3x1}")`
-  });
 }
 
 export default HeroStandard3x1;
